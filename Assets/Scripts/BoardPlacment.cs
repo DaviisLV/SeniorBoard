4a83442b3000a3506f521f1764c97256647e036c
DaviisLV <abols.davis@gmail.com>
2018-04-28 01:28:35 +0300

    add debug

--- Assets/Scripts/BoardPlacment.cs
+++ Assets/Scripts/BoardPlacment.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardPlacment : MonoBehaviour {
+public class BoardPlacment : MonoBehaviour
+{
 
     public GameObject Board;
     public GameObject ControllerLeft;
     public GameObject ControllerRight;
 
-	void Start () {
-       
+    void Start()
+    {
+        SetBoardPosition();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		 SetBoardPosition();
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void FixedUpdate()
     {
@@ -25,12 +28,17 @@ public class BoardPlacment : MonoBehaviour {
 
     private void SetBoardPosition()
     {
-       Board.transform.position = ControllerLeft.transform.position + (ControllerRight.transform.position - ControllerLeft.transform.position) / 2;
+
+        float pozX = ControllerLeft.transform.position.x + (ControllerRight.transform.position.x - ControllerLeft.transform.position.x) / 2;
+        float pozZ = ControllerLeft.transform.position.z + (ControllerRight.transform.position.z - ControllerLeft.transform.position.z) / 2;
+        float pozY = ControllerLeft.transform.position.y;
+
+        Board.transform.position = new Vector3(pozX, pozY, pozZ);
     }
 
     private void ShowControllerPoz()
     {
-        Debug.Log("Right X = "+ ControllerRight.transform.position.x+ "Right Z = " + ControllerRight.transform.position.z);
+        Debug.Log("Right X = " + ControllerRight.transform.position.x + "Right Z = " + ControllerRight.transform.position.z);
         Debug.Log("Left X = " + ControllerLeft.transform.position.x + "Left Z = " + ControllerLeft.transform.position.z);
     }
 

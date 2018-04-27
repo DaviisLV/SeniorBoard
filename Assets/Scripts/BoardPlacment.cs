181d4cc9677132ab7180d1d2561a65fcebf7bc09
DaviisLV <abols.davis@gmail.com>
2018-04-28 00:13:12 +0300

    add bord placment

---
+++ Assets/Scripts/BoardPlacment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardPlacment : MonoBehaviour {
+
+    public GameObject Board;
+    public GameObject ControllerLeft;
+    public GameObject ControllerRight;
+
+	void Start () {
+        GetBoardPosition();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GetBoardPosition()
+    {
+       Board.transform.position = ControllerLeft.transform.position + (ControllerRight.transform.position - ControllerLeft.transform.position) / 2;
+
+        Debug.Log(ControllerRight.transform.position);
+        Debug.Log(ControllerLeft.transform.position);
+        Debug.Log(Board.transform.position);
+    }
+
+}

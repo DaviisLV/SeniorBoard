66564d47b285734b3ca9f8cbc9372557552db0ce
DaviisLV <abols.davis@gmail.com>
2018-04-28 04:10:21 +0300

    add changes

--- Assets/Scripts/BoardPlacment.cs
+++ Assets/Scripts/BoardPlacment.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class BoardPlacment : MonoBehaviour
 {
-    public GameObject coin;
+    public GameObject Box;
+        
     public GameObject Board;
     public GameObject ControllerLeft;
     public GameObject ControllerRight;
+    public GameObject Head;
 
     void Start()
     {
-        coin.transform.position = new Vector3(coin.transform.position.x, ControllerRight.transform.position.y, coin.transform.position.z);
+
 
     }
 
@@ -19,6 +21,7 @@ public class BoardPlacment : MonoBehaviour
     void Update()
     {
         SetBoardPosition();
+        SetBoxPoz();
     }
 
     private void FixedUpdate()
@@ -36,6 +39,12 @@ public class BoardPlacment : MonoBehaviour
         Board.transform.position = new Vector3(pozX, pozY, pozZ);
     }
 
+    private void SetBoxPoz()
+    {
+
+        Box.transform.position = new Vector3(Board.transform.position.x+5, Board.transform.position.y, Board.transform.position.z);  
+
+    }
     private void ShowControllerPoz()
     {
         Debug.Log("Right X = " + ControllerRight.transform.position.x + "Right Z = " + ControllerRight.transform.position.z);

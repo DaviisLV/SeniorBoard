e140754dcc95a4aac2bd566b112a01183998185b
DaviisLV <abols.davis@gmail.com>
2018-04-28 19:37:15 +0300

    all commits ar the same

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -12,10 +12,12 @@ public class MoveObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (move)
+        if (move) 
         transform.position += Vector3.back * Time.deltaTime*speed;
         if (!move)
             transform.position += Vector3.up * Time.deltaTime*7;
+
+        transform.Rotate(Vector3.back * Time.deltaTime* 100);
     }
 
     public void stop()

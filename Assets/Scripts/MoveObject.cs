d9f90afb052f0a055e800b7844e96692f9bd12d4
DaviisLV <abols.davis@gmail.com>
2018-04-28 18:25:04 +0300

    all commits ar the same

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour {
     public bool move = true;
+    public float speed = 1;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,9 +13,9 @@ public class MoveObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (move)
-        transform.position += Vector3.back * Time.deltaTime;
+        transform.position += Vector3.back * Time.deltaTime*speed;
         if (!move)
-            transform.position += Vector3.up * Time.deltaTime*10;
+            transform.position += Vector3.up * Time.deltaTime*7;
     }
 
     public void stop()

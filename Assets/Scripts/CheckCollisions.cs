aee67c5ba39797cce73a75a9f0bdeba04893ef30
DaviisLV <abols.davis@gmail.com>
2018-04-28 23:34:50 +0300

    all commits ar the same

--- Assets/Scripts/CheckCollisions.cs
+++ Assets/Scripts/CheckCollisions.cs
@@ -20,7 +20,7 @@ public class CheckCollisions : MonoBehaviour {
         Debug.Log("HIT");
         if (other.gameObject.tag == "Coin")
         {
-            audioSource.PlayOneShot(impact, 0.7F);
+            audioSource.PlayOneShot(impact);
             _hitCount++;
             other.gameObject.GetComponent<MoveObject>().stop();
             Destroy(other.gameObject,0.7f);

dc9b99c6380946fdd22b72023d0aac0f2d16a04f
DaviisLV <abols.davis@gmail.com>
2018-04-28 15:56:32 +0300

    add changes

--- Assets/Scripts/GenereitObjects.cs
+++ Assets/Scripts/GenereitObjects.cs
@@ -3,28 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GenereitObjects : MonoBehaviour {
-
+    public Vector3[] path;
     public GameObject ob;
     public Transform startPos;
     Vector3 poz;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+
+    private void Awake()
+    {
+        for (int i = 0; i < path.Length; i++)
+        {
+            path[i].y = startPos.position.y;
+        }
+    }
+    void Start () {
 
         poz = startPos.position;
 
-        InvokeRepeating("Go", 2, 2);
-
+        //  InvokeRepeating("Go", 2, 2);
+        Go();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+     
 	}
 
     public void Go()
     {
-        GameObject newBut = Instantiate(ob, poz, transform.rotation) as GameObject;
-
+        for (int i = 0; i < path.Length; i++)
+        {
+            GameObject newBut = Instantiate(ob, path[i], transform.rotation) as GameObject;
+            newBut.transform.parent = startPos;
+            newBut.transform.position = path[i];
+            Debug.Log(newBut.transform.position);
+        }
 
     }
 }
